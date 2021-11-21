4c7e7e99bac4e2cba846d3513878784a33675331
Rasmus Heikkinen <rasmus.heikkinen@helsinki.fi>
2021-11-21 23:05:19 +0200

    Add first functionalities

--- app.py
+++ app.py
@@ -1,3 +1,35 @@
 from flask import Flask
+from flask import redirect, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from os import getenv
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+db = SQLAlchemy(app)
+
+@app.route("/")
+def index():
+    sql = "SELECT id, name, totaltime FROM activities ORDER by totaltime DESC"
+    result = db.session.execute(sql)
+    activities = result.fetchall()
+    return render_template("index.html", activities=activities)
+
+@app.route("/new-activity")
+def newactivity():
+    return render_template("new-activity.html")
+
+@app.route("/create-activity", methods=["POST"])
+def create_activity():
+    name = request.form["name"]
+    sql = "INSERT INTO activities (name, totaltime) values (:name, NOW() - NOW())"
+    result = db.session.execute(sql, {"name":name})
+    db.session.commit()
+    return redirect("/")
+
+#@app.route("/activity-entry", methods=["POST"])
+#def activityentry():
+#    return render_template("activity-entry.html")
+
+def add_entry():
+    pass
+
