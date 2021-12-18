ecd65a232ec95f8ab456a780175ff1ddf711a452
Rasmus Heikkinen <rasmus.heikkinen@helsinki.fi>
2021-12-18 17:02:09 +0200

    Add a layout for HTML templates

--- routes.py
+++ routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template, request, redirect, abort
+from flask import render_template, request, redirect, abort, flash
 import users, categories, activities, entries
 
 @app.route("/")
