ce0272b4a6e633c94d7608449783800aea8fd00f
Rasmus Heikkinen <rasmus.heikkinen@helsinki.fi>
2021-12-19 21:42:49 +0200

    Remove unused import

--- routes.py
+++ routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template, request, redirect, abort, flash
+from flask import render_template, request, redirect, abort
 import users, categories, activities, entries
 
 @app.route("/")
